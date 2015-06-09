bd9feb28724a0d33fbd91043d0151aae9a9a31db
Alexey Alekhin <laughedelic@gmail.com>
2015-06-09 12:13:58 +0200

    Quick example of rewriting GADTs

--- src/main/scala/Rephrase.scala
+++ src/main/scala/Rephrase.scala
@@ -1,5 +1,61 @@
-package ohnosequences.rephrase
+package ohnosequences
 
-class Rephrase {
+case object lang {
 
+  sealed trait AnyExpr { type Repr }
+  trait Expr[R] extends AnyExpr { type Repr = R }
+
+  sealed trait BoolExpr extends Expr[Boolean]
+  sealed trait IntExpr extends Expr[Int]
+
+  case class BoolVar(sym: Symbol) extends BoolExpr
+  case class Not[E <: BoolExpr](e: E) extends BoolExpr
+  case class And[L <: BoolExpr, R <: BoolExpr](l: L, r: R) extends BoolExpr
+  case class Or[L <: BoolExpr, R <: BoolExpr](l: L, r: R) extends BoolExpr
+
+  case class IntIte[C <: BoolExpr, T <: IntExpr, E <: IntExpr](cond: C, thenExpr: T, elseExpr: E) extends IntExpr
+  case class IntConst(value: Int) extends IntExpr
+  case class IntVar(sym: Symbol) extends IntExpr
+
+}
+
+case object rephrase {
+  import lang._
+
+  trait AnyRephrase {
+
+    type InExpr <: AnyExpr
+    type OutExpr <: AnyExpr { type Repr = InExpr#Repr }
+
+    def apply(expr: InExpr): OutExpr
+  }
+
+  trait Rephrase[IE <: AnyExpr, OE <: AnyExpr { type Repr = IE#Repr }] extends AnyRephrase {
+
+    type InExpr = IE
+    type OutExpr = OE
+  }
+
+  def rephrase[IE <: AnyExpr, OE <: AnyExpr { type Repr = IE#Repr }](e: IE)
+    (implicit rephr: Rephrase[IE, OE]): OE = rephr(e)
+}
+
+
+case object rules {
+  import lang._
+  import rephrase._
+
+  implicit def doubleNegation[E <: BoolExpr]:
+      Rephrase[Not[Not[E]], E] =
+  new Rephrase[Not[Not[E]], E] {
+
+    def apply(expr: InExpr): OutExpr = expr.e.e
+  }
+
+  implicit def switchCondition[C <: BoolExpr, T <: IntExpr, E <: IntExpr]:
+      Rephrase[IntIte[Not[C], T, E], IntIte[C, E, T]] =
+  new Rephrase[IntIte[Not[C], T, E], IntIte[C, E, T]] {
+
+    def apply(expr: InExpr): OutExpr = IntIte(expr.cond.e, expr.elseExpr, expr.thenExpr)
+  }
 }
--- src/test/scala/Rephrase.scala
+++ src/test/scala/Rephrase.scala
@@ -1,16 +1,30 @@
-package ohnosequences.rephrase.test
+package ohnosequences.test
 
-import org.scalatest.FunSuite
-
+import ohnosequences.lang._
 import ohnosequences.rephrase._
 
-class RephraseTest extends FunSuite {
+class RephraseTest extends org.scalatest.FunSuite {
 
-  test("Dummy test coming from the template") {
+  test("rewriting double negation") {
+    import ohnosequences.rules._
 
-    assert(
+    val boo = BoolVar('boo)
 
-      12 === 12
-    )
+    val neg2 = Not(Not(boo))
+    assert{ rephrase(neg2) == boo }
+
+    val neg3 = Not(neg2)
+    assert{ rephrase(neg3) == Not(boo) }
+  }
+
+  test("rewriting if-then-else") {
+    import ohnosequences.rules._
+
+    val c = BoolVar('c)
+    val t = IntVar('t)
+    val f = IntVar('f)
+
+    val ite = IntIte(Not(c), t, f)
+    assert{ rephrase(ite) == IntIte(c, f, t) }
   }
 }
